e1066baac17dfcf6c7d5401ed47db8494caafe41
mochimochinoki <yasu@y-no-MacBook-Air.local>
2017-05-29 13:13:59 +0900

    課題５

---
+++ SlideshowApp/EnlargeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  EnlargeViewController.swift
+//  SlideshowApp
+//
+//  Created by yamamoto yasuhiro on 2017/05/25.
+//  Copyright © 2017年 mochimochinoki. All rights reserved.
+//
+
+import UIKit
+
+class EnlargeViewController: UIViewController {
+    var imagea = UIImage()
+    @IBOutlet weak var enlargeView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        enlargeView.image = imagea
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,17 +9,98 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var goButton: UIBarButtonItem!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var startstop: UIBarButtonItem!
+    let image1 = UIImage(named: "forest.jpg")!
+    let image2 = UIImage(named: "mountain.jpg")!
+    let image3 = UIImage(named: "lightbulb.jpg")!
+    var timer: Timer!
+    func setButtonEnabled(startstop:Bool){
+        self.goButton.isEnabled = startstop
+        self.backButton.isEnabled = startstop
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        image.image = image1
+        image.isUserInteractionEnabled = true
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func updatephoto(){
+        if image.image == image1{
+            image.image = image2
+        }else if image.image == image2{
+            image.image = image3
+        }else{
+            image.image = image1
+        }
+        
+    }
+    
+    
+    @IBAction func nextPhoto(_ sender: Any) {
+        if image.image == image1{
+            image.image = image2
+        }else if image.image == image2{
+            image.image = image3
+        }else{
+            image.image = image1
+        }
+        
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        if image.image == image1{
+            image.image = image3
+        }else if image.image == image2{
+            image.image = image1
+        }else{
+            image.image = image2
+        }
+        
+    }
+    @IBAction func startstopButton(_ sender: Any) {
+        if self.timer == nil{
+            self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updatephoto), userInfo: nil, repeats: true)
+            setButtonEnabled(startstop: false)
+        }else{
+            self.timer.invalidate()
+            setButtonEnabled(startstop: true)
+            self.timer = nil
+        }
+        
+    }
+    @IBAction func tapimage(_ sender: Any) {
+        performSegue(withIdentifier: "toEnlargeViewController" , sender: nil)
+        if self.timer != nil{
+            self.timer.invalidate()
+            self.timer = nil
+        }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any!) {
+        if segue.identifier == "toEnlargeViewController"{
+            let subVC = segue.destination as! EnlargeViewController
+            if image.image == image1{
+                subVC.imagea = self.image1
+            }else if image.image == image2{
+                subVC.imagea = self.image2
+            }else if image.image == image3{
+                subVC.imagea = self.image3
+            }
+        }
+    }
+    @IBAction func returnToTop(segue: UIStoryboardSegue){
+        if self.timer == nil{
+            self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updatephoto), userInfo: nil, repeats: true)
+        }
+    }
 }
-
